f684684236667f3b4183232bb06a8a46143f38a0
saimon0 <szymen9712@gmail.com>
2021-12-04 17:35:27 +0100

    working encryption and decryption for 80 bit key

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ s_box = {
     '1110': '0001', '1111': '0010',
 }
 
+
 p_box = [
     '0', '16', '32', '48', '1', '17', '33', '49',
     '2', '18', '34', '50', '3', '19', '35', '51',
@@ -23,26 +24,22 @@ p_box = [
 ]
 
 
-def convert_to_bits(a):
-    return ' '.join(format(ord(x), 'b') for x in a)
+rp_box = [
+    '0', '16', '32', '48', '1', '17', '33', '49',
+    '2', '18', '34', '50', '3', '19', '35', '51',
+    '4', '20', '36', '52', '5', '21', '37', '53',
+    '6', '22', '38', '54', '7', '23', '39', '55',
+    '8', '24', '40', '56', '9', '25', '41', '57',
+    '10', '26', '42', '58', '11', '27', '43', '59',
+    '12', '28', '44', '60', '13', '29', '45', '61',
+    '14', '30', '46', '62', '15', '31', '47', '63',
+]
 
 
 K = ''
 blocks = []
 round_keys = []
 round_counter = 1
-
-
-ind = 0
-for a in range(80):
-    if ind == 19:
-        #bit = 1
-        bit = 0
-    else:
-        # bit = random.randint(0, 1)
-        bit = 0
-    K = K + str(bit)
-    ind += 1
 
 
 def offset_bits(key):
@@ -88,8 +85,10 @@ def generate_round_keys():
 
 def get_plaintext_blocks(input):
     global blocks
-    bits = ''.join(format(ord(i), '08b') for i in input)
-    #bits = input
+    if input.isdigit():
+        bits = input
+    else:
+        bits = ''.join(format(ord(i), '08b') for i in input)
     blocks = []
     while bits:
         blocks.append(bits[:64])
@@ -114,7 +113,7 @@ def make_subsitution(block):
         s_boxes.append(block[:4])
         block = block[4:]
     for x in s_boxes:
-        changed_block += s_box.get(x)
+        changed_block += s_box.get(str(x))
     return changed_block
 
 
@@ -126,7 +125,37 @@ def make_permutation(block):
     return permutated_block
 
 
+def reverse_permutation(block):
+    block = list(block)
+    repermutated_block = list(block)
+    for index, p_index in enumerate(p_box):
+        repermutated_block[int(index)] = block[int(p_index)]
+    return repermutated_block
+
+
+def find_key(input_dict, value):
+    result = "None"
+    for key, val in input_dict.items():
+        if val == value:
+            result = key
+    return result
+
+
+def reverse_subsitution(block):
+    s_boxes = []
+    changed_block = ''
+    while block:
+        s_boxes.append(block[:4])
+        block = block[4:]
+    for x in s_boxes:
+        reversed_sub = find_key(s_box, str(x))
+        changed_block += str(reversed_sub)
+    return changed_block
+
+
 def encrypt():
+    get_plaintext_blocks(plaintext)
+    generate_round_keys()
     global blocks
     enc_d = ''
     encrypted_data = ''
@@ -149,17 +178,52 @@ def encrypt():
     return enc_d
 
 
+def decrypt(ciphertext):
+    global round_keys
+    ct_arr = []
+    decrypted_data = ''
+    round_keys_arr = round_keys[1:]
+    while ciphertext:
+        ct_arr.append(ciphertext[:64])
+        ciphertext = ciphertext[64:]
+    for b in ct_arr:
+        for rk in reversed(round_keys_arr):
+            b = add_round_key(b, rk)
+            b = reverse_permutation(b)
+            b = reverse_subsitution(''.join(b))
+        add_round_key(b, round_keys[0])
+        decrypted_data += str(b)
+    return decrypted_data
+
+
+def binary_to_ascii(text):
+    text_arr = []
+    while text:
+        text_arr.append(text[:8])
+        text = text[8:]
+    for i, x in enumerate(text_arr):
+        text_arr[i] = chr(int(x[:8], 2))
+    ascii_text = ''.join(text_arr).rstrip('\x00')
+    return ascii_text
+
+
+def verify_key_length(key):
+    if len(key) != 80 or not isinstance(key, int):
+        print("Key must be 10 integer-digit number!")
+
 K = '00000000000000000000000000000000000000000000000000000000000000000000000000000000'
-#K = '11111111111111111111111111111111111111111111111111111111111111111111111111111111'
-print("KEY: " + str(K))
 
-#plaintext = '0000000000000000000000000000000000000000000000000000000000000000'
-#plaintext = '1111111111111111111111111111111111111111111111111111111111111111'
-plaintext = 'Eloeloelo'
+plaintext = 'tajna wiadomosc zaszyfrowana szyfrem PRESENT'
 
-get_plaintext_blocks(plaintext)
-generate_round_keys()
+
 encrypted_data = encrypt()
-print('\nencrypted data:\n')
-print(encrypted_data)
-print(hex(int(encrypted_data, 2)))
+
+print("\nKEY: " + str(K))
+print("PLAINTEXT: " + str(plaintext))
+print("\nCIPHERTEXT (BIN): " + str(encrypted_data))
+print("CIPHERTEXT (HEX): " + hex(int(encrypted_data, 2)))
+
+decrypted_data = decrypt(encrypted_data)
+
+print("\nDECRYPTED PLAINTEXT (BIN): " + str(decrypted_data))
+print("DECRYPTED PLAINTEXT (ASCII): " + (binary_to_ascii(decrypted_data)))
